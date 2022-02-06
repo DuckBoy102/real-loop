7ff242385c6dcf8a532e1c0f330c4585aa47d6f5
DuckBoy102 <avieln7@gmail.com>
2022-02-06 17:02:07 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 3; i++)
+            {
+                Console.WriteLine(i * 5);
+            }
+            if (i >= 3)
+                Console.WriteLine("false");
+            else
+                Console.WriteLine("true");
+        }
+    }
+}
